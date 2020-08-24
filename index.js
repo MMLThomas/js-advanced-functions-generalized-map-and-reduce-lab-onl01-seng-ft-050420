815674927e542ad6a8f4e2b6612c3abe4286cba6
michael thomas <michael.thomas2014@gmail.com>
2020-08-24 23:50:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+
+function map(array,func){
+    const newArray = []
+    for (let i=0; i < array.length; i++) {
+        newArray.push(func(array[i]))
+    }
+    return newArray
+}
+
+
+
+function reduce(arr, func, start=0) {
+    let rt = !!start ? start : arr[0]
+    let i = !!start ? 0 : 1
+    for ( ; i < arr.length; i++) {
+        rt = func(arr[i], rt)
+    }
+    return rt
+}
+
+
